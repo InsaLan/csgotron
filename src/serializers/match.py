44de48a6e0198d2b559157f44d528ef487c479db
Navatar <teo.nespoulet@gmail.com>
2020-12-13 14:46:50 +0100

    [resolving merging issues]

--- src/serializers/match.py
+++ src/serializers/match.py
@@ -7,43 +7,17 @@ from .team import TeamSchema
 
 
 class BaseMatch(Schema):
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  id = fields.Int(strict=True, dump_only=True)
-  map = fields.Str(required=True)
-
-  password = fields.Str(required=True)
-  maxRound = fields.Int(strict=True)
-=======
   id = fields.Int()
   map = fields.Str(required=True)
 
   maxRound = fields.Int()
->>>>>>> 682c9c8 ([feat] start ApiUser)
-=======
-  id = fields.Int()
-  map = fields.Str(required=True)
-
-  maxRound = fields.Int()
->>>>>>> 682c9c8c09640026f46c9a5adf69f66d9fdb7a19
-
   overtime = fields.Bool(missing=True)
   knifeRound = fields.Bool(missing=True)
   streamerReady = fields.Bool(missing=False)
   playAllRound = fields.Bool(missing=True)
   autostartMatch = fields.Bool(missing=True)
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-  maxRound = fields.Int(strict=True)
-
-=======
-=======
->>>>>>> 682c9c8c09640026f46c9a5adf69f66d9fdb7a19
   maxRound = fields.Int()
   
->>>>>>> 682c9c8 ([feat] start ApiUser)
   @post_load
   def make_match(self, data, **kwargs):
     return Match(**data)
@@ -53,14 +27,7 @@ class BaseMatch(Schema):
 # and we allow the user to fill the Team IDs
 # TODO: this whole approch with two derived classes can be replaced by dump_only and load_only
 class MatchRequestSchema(BaseMatch):
-<<<<<<< HEAD
-=======
   password = fields.Str(required=True)
-
-<<<<<<< HEAD
->>>>>>> 682c9c8 ([feat] start ApiUser)
-=======
->>>>>>> 682c9c8c09640026f46c9a5adf69f66d9fdb7a19
   idTeamA = fields.Int(required=True)
   idTeamB = fields.Int(required=True)
   idServer = fields.Int(required=True)
