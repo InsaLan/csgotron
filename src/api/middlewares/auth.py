bcfd543e8d4bb66826e99d90f6ac0e87e6b19e61
Navatar <teo.nespoulet@gmail.com>
2020-12-13 14:23:43 +0100

    [feat] adding error middleware

--- src/api/middlewares/auth.py
+++ src/api/middlewares/auth.py
@@ -1,11 +1,21 @@
 from aiohttp import web
-def auth(func):
-    func.auth = True
+from inspect import isfunction
+def auth_required(func):
+    func.isAuth = True
     return func
 
 @web.middleware
 async def auth_middleware(request, handler):
     response = await handler(request)
-    if not hasattr(handler, "auth"):
+    if isfunction(handler):
+        auth_req = hasattr(handler, "isAuth")
+    else:
+        auth_req = hasattr(getattr(handler, request.method.lower()),"isAuth")
+    if auth_req:
+        try:
+            auth = request.headers['Authorization']
+            return web.json_response({'success': 'Authorization header found'}, status=200)
+        except:
+            return web.json_response({'error': 'missing authentification header'}, status=401)
+    else:
         return response
-    return web.json_response({'error':  'not auth'}, status=403)
--- src/loop.py
+++ src/loop.py
@@ -1,15 +1,24 @@
 from aiohttp import web
 from src.db import models as db
+<<<<<<< HEAD
 from src.api.middlewares.exception import error_middleware
 
 def setup_aio():
   db.create_schema()
   app = web.Application(middlewares=[error_middleware])
+=======
+from src.api.middlewares.auth import auth_middleware, error_middleware
 
-  from src.api import match, server, team
+def setup_aio():
+  db.create_schema()
+  app = web.Application(middlewares=[auth_middleware, error_middleware])
+>>>>>>> 6a2cd46 ([feat] improving auth middleware)
+
+  from src.api import match, server, team, ApiUser
   app.router.add_routes(match.routes)
   app.router.add_routes(server.routes)
   app.router.add_routes(team.routes)
+  app.router.add_routes(ApiUser.routes)
   
   return app
 
