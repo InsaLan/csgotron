4bc67a8f7bb742068000e0637b7092f3f4f1ef97
Saltztraumen <a@a>
2020-12-23 17:06:56 +0100

    ajout de token

--- src/message/parser.py
+++ src/message/parser.py
@@ -3,6 +3,27 @@ import ply.lex as lex
 class MonLexer(object):
 
     tokens = ( 
+        'ATTACKED',
+        'HITGROUP',
+        'ATTACK',
+        'DEFUSEBOMB',
+        'SFUI',
+        'PLANTBOMB',
+        'GOTBOMB',
+        'ENTERED',
+        'TRIGGERED',
+        'ROUND_END',
+        'ROUND_START',
+        'ROUND_SPAWN',
+        'SAY',
+        'SAYTEAM',
+        'SCORED',
+        'T',
+        'P',
+        'TO',
+        'SWITCHTEAM',
+        #'JOINTEAM',
+        'THREW',
         'KILLED',
         'WITH',
         'LOWER',
@@ -11,17 +32,39 @@ class MonLexer(object):
         'NUMBER',
         'TEAM',
         'TEAMQ',
+        'QTEAMQ',
         'WEAPON',
         'ASSIST',
-        'NAME',
-        'STEAMID',
+        'STUFF',
         #'ID',
         'DQUOTE',
+        'NAME',
+        'STEAMID',
+        #"SPACE",
 
     )
-
+    t_ATTACKED = r'\sattacked\s'
+    t_HITGROUP = r'\s\(hitgroup\s"[a-z\s]+"\)'
+    t_ATTACK = r'\s\((health|damage|damage_armor|armor)\s"[0-9]+"\)'
+    t_DEFUSEBOMB = r'"Begin_Bomb_Defuse_With(out)?_Kit"'
+    t_PLANTBOMB = r'"Planted_The_Bomb"'
+    t_ENTERED = r'\sentered\sthe\sgame'
+    t_GOTBOMB = r'"Got_The_Bomb"'
+    t_SFUI = r'"SFUI_Notice_(Terrorists_Win|CTs_Win|Target_Bombed|Target_Saved|Bomb_Defused)"'
+    t_TRIGGERED = r'\strigerred\s'
+    t_ROUND_SPAWN = r'eBot\striggered\s"Round_Spawn"'
+    t_ROUND_START = r'World\striggered\s"Round_Start"'
+    t_ROUND_END = r'World\striggered\s"Round_End"'
+    t_T = r'Team\s'
+    t_P = r'\splayers'
+    t_SCORED = r'\sscored\s'
+    t_QTEAMQ = r'"(CT|TERRORIST)"'
+    t_SWITCHTEAM = r'\sswitched\sfrom\steam\s'
+    t_TO = r'\sto\s'
+    t_STUFF = r'hegrenade|flashbang|smokegrenade|decoy|molotov'
+    t_THREW = r'\sthrew\s'
     t_ASSIST = r'\sassisted\skilling\s'
-    t_WEAPON = r'"[A-Z][a-zA-Z0-9\s]+"'
+    t_WEAPON = r'"[A-Za-z][a-zA-Z0-9\s_]+"'
     t_KILLED = r'\skilled\s'
     t_WITH = r'\swith\s' 
     t_TEAM = r'<(TERRORIST|CT|Unassigned|Spectator)>'
@@ -29,11 +72,15 @@ class MonLexer(object):
     t_LOWER = r'<'
     t_UPPER = r'>'
     t_NUMBER = r'[0-9]+'
-    t_POS = r'\[-?[0-9]+\s-?[0-9]+\s-?[0-9]+\]'
-    t_NAME = r'".{2,32}<[0-9]>'
-    #t_ID = r'[0-9]+'
-    t_STEAMID = r'[^<>]+'
+    t_POS = r'\s\[-?[0-9]+\s-?[0-9]+\s-?[0-9]+\]'
     t_DQUOTE = r'"' 
+    t_NAME = r'".{2,32}<[0-9]{1,2}>'
+    #t_ID = r'[0-9]+'
+    t_SAYTEAM = r'\ssay_team\s".*"'
+    t_SAY = r'\ssay\s".*"'
+    t_STEAMID = r'(BOT|STEAM_[0-9:]+)'
+    #t_SPACE = r'\s'
+    
 
 
 
--- src/message/test.py
+++ src/message/test.py
@@ -2,4 +2,9 @@ import parser as p
 
 m= p.MonLexer()
 m.build()
-m.test('"Clarence<4><BOT><CT>" assisted killing "Clarence<3><BOT><TERRORIST>"')
+m.test('"Claren<4><123><CT>" assisted killing "Clarence<3><BOT><TERRORIST>"'  )
+m.test('"Adrian<14><BOT>" switched from team <Unassigned> to <TERRORIST>')
+m.test('"Ivan<20><BOT><CT>" [-11960 4192 3584] attacked "Jeff<22><BOT><TERRORIST>" [-11282 3856 3585] with "hkp2000" (damage "19") (damage_armor "9") (health "21") (armor "70") (hitgroup "stomach")')
+m.test('"Hank<21><BOT><CT>" [-11280 7479 3585] killed "Chet<18><BOT><TERRORIST>" [-11450 7628 3648] with "usp_silencer"')
+m.test('eBot triggered "Round_Spawn"')
+m.test('World triggered "Round_Start"')
