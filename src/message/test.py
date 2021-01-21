d846cddf58f09275b27b2a7dfbe8ffd7d50e3898
Saltztraumen <a@a>
2021-01-21 14:51:37 +0100

    début du parseur

--- src/message/test.py
+++ src/message/test.py
@@ -1,6 +1,7 @@
 import parser as p
+import lexer as l
 
-m= p.MonLexer()
+m= l.MonLexer()
 m.build()
 m.test('"Claren<4><123><CT>" assisted killing "Clarence<3><BOT><TERRORIST>"'  )
 m.test('"Adrian<14><BOT>" switched from team <Unassigned> to <TERRORIST>')
@@ -8,3 +9,6 @@ m.test('"Ivan<20><BOT><CT>" [-11960 4192 3584] attacked "Jeff<22><BOT><TERRORIST
 m.test('"Hank<21><BOT><CT>" [-11280 7479 3585] killed "Chet<18><BOT><TERRORIST>" [-11450 7628 3648] with "usp_silencer"')
 m.test('eBot triggered "Round_Spawn"')
 m.test('World triggered "Round_Start"')
+n= p.MonParser()
+n.test('"Claren<4><BOT><CT>" assisted killing "Clarence<3><BOT><TERRORIST>"')
+n.test('"Ivan<20><BOT><CT>" [-11960 4192 3584] attacked "Jeff<22><BOT><TERRORIST>" [-11282 3856 3585] with "hkp2000" (damage "19") (damage_armor "9") (health "21") (armor "70") (hitgroup "stomach")')
